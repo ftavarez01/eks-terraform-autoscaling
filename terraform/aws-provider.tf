9e414f21d80545aba2a64b3def7d2648119499d6
Francisco Tavarez <francisco.tavarezveras@example.com>
2025-09-18 16:54:46 -0400

    feat: adding GitHub Actions workflow for Terraform CI/CD

--- terraform/aws-provider.tf
+++ terraform/aws-provider.tf
@@ -1,3 +1,4 @@
+/*
 # Providers AWS and necesary verson and regions.
 terraform {
   required_providers {
@@ -11,4 +12,25 @@ terraform {
 provider "aws" {
   # Configuration options
    region = var.region
+}
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.29.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.27.1"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-2"
+  shared_config_files      = ["/home/fsociety/.aws/config"]
+  shared_credentials_files = ["/home/fsociety/.aws/credentials"]
+  profile                  = "default"
 }
